59e254af86437003c0d23a125bfd011bc9c9cbb0
Lucas Esteves <lucas.esteves.rocha@gmail.com>
2022-09-30 14:36:03 +0200

    Updating ccr func

--- source/amp_utils.c
+++ source/amp_utils.c
@@ -26,15 +26,14 @@ uint32_t amp_millis(void)
 
 
 /**
- * @brief Function to perform multiple reads or writes in a given memory address
+ * @brief Function to perform multiple reads in a given memory address
  * 
- * @param data_out  The pointer where the data are going to be stored 
  * @param data_in   The pointr to the data that will be read
- * @param len       Size to be read/written
- * @param ccr_n     How many times to perform the read/write
+ * @param len       Size to be read
+ * @param ccr_n     How many times to perform the read
  * @return int      Returns < 0 if an error ocurred, 0 if success
  */
-int force_ccr_data(uint8_t *data_in, size_t len, int ccr_n)
+int force_ccr_read_data(uint8_t *data_in, size_t len, int ccr_n)
 {
     // Checking input parameters
 
@@ -51,4 +50,43 @@ int force_ccr_data(uint8_t *data_in, size_t len, int ccr_n)
     }
     
     return 0;
+}
+
+/**
+ * @brief Function to perform multiple writes in a given memory address
+ * 
+ * @param data_in   The pointr to the data that will be written
+ * @param len       Size to be written
+ * @param ccr_n     How many times to perform the write
+ * @return int      Returns < 0 if an error ocurred, 0 if success
+ */
+int force_ccr_write_data(uint8_t *data_in, size_t len, int ccr_n)
+{
+    // Checking input parameters
+
+    if (data_in == NULL && len > 0) {
+        return -1;
+    }
+
+    uint8_t * data_temp = malloc(len);
+    
+    if(data_temp == NULL)
+    {
+        return -2;
+    }
+
+    /* Copying data */
+    for(int j = 0; j < len; j++){
+        data_temp[j] = data_in[j];
+    } 
+
+    /* Performing ccr write */
+    for (int i = 0; i < ccr_n; i++) {
+        for(int j = 0; j < len; j++){
+            data_in[j] = data_temp[j];
+        } 
+    }
+    
+    return 0;
+
 }
